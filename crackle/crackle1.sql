9f9007cd256361c97dd1a8e55ebe9d55f5dcb1f1
shuchi <shuchi.khandelwal@gmail.com>
2021-08-25 19:05:44 -0500

    primesql

---
+++ crackle/crackle1.sql
@@ -0,0 +1,56 @@
+CREATE TABLE crackle_table (
+          IMDB_id VARCHAR PRIMARY KEY,
+	      title VARCHAR,
+	      year VARCHAR,
+	      rated VARCHAR,
+	      released INT,
+	      runtime INT,
+	      genres TEXT,
+	      director TEXT,
+	      writer TEXT,
+	      actors TEXT,
+	      language TEXT,
+	      country TEXT,
+	      imdb_Ratings INT,
+	      metacritic_rating INT,
+	      types TEXT
+
+);
+
+CREATE TABLE netfilx_table (
+          IMDB_id VARCHAR PRIMARY KEY,
+	      title VARCHAR,
+	      year VARCHAR,
+	      rated VARCHAR,
+	      released INT,
+	      runtime INT,
+	      genres TEXT,
+	      director TEXT,
+	      writer TEXT,
+	      actors TEXT,
+	      language TEXT,
+	      country TEXT,
+	      imdb_Ratings INT,
+	      metacritic_rating INT,
+	      types TEXT
+
+);
+
+CREATE TABLE disney_table (
+          IMDB_id VARCHAR PRIMARY KEY,
+	      title VARCHAR,
+	      year VARCHAR,
+	      rated VARCHAR,
+	      released INT,
+	      runtime INT,
+	      genres TEXT,
+	      director TEXT,
+	      writer TEXT,
+	      actors TEXT,
+	      language TEXT,
+	      country TEXT,
+	      imdb_Ratings INT,
+	      metacritic_rating INT,
+	      types TEXT
+
+);
