86d06957dcb3df44d811a157e259b0741a48882b
Dasha <84484371+DasaSimo@users.noreply.github.com>
2021-08-27 19:02:58 -0400

    updating queries

--- sample_queries.sql
+++ sample_queries.sql
@@ -16,3 +16,41 @@ SELECT m.imdb_id, title, actors, source FROM moviesandshows m
 JOIN moviesource a on m.imdb_id=a.imdb_id
 JOIN source p on p.source_id= a.source_id
 WHERE actors LIKE '%Samuel%Jackson%';
+
+--select movies from all platform by actors name - in this case Samuel Jackson OR Chris Evans 
+SELECT m.imdb_id, title, actors, source FROM moviesandshows m
+JOIN moviesource a on m.imdb_id=a.imdb_id
+JOIN source p on p.source_id= a.source_id
+WHERE actors LIKE '%Samuel%Jackson%'
+OR actors LIKE '%Chris Evans%';
+
+--select movies from all platform by actors name - in this case Samuel Jackson AND Chris Evans 
+SELECT m.imdb_id, title, actors, source FROM moviesandshows m
+JOIN moviesource a on m.imdb_id=a.imdb_id
+JOIN source p on p.source_id= a.source_id
+WHERE actors LIKE '%Samuel%Jackson%'
+AND actors LIKE '%Chris Evans%';
+
+
+--select movies by year - in this case 2020
+SELECT m.imdb_id, title, year, source FROM moviesandshows m
+JOIN moviesource a on m.imdb_id=a.imdb_id
+JOIN source p on p.source_id= a.source_id
+WHERE year = '2020';
+
+--select movies where rating is higher than .. - in this case 8.5, higher rating first 
+SELECT m.imdb_id, title, imdb_rating, source FROM moviesandshows m
+JOIN moviesource a on m.imdb_id=a.imdb_id
+JOIN source p on p.source_id= a.source_id
+WHERE imdb_rating >= '8.5'
+AND imdb_rating <> 'N/A'
+ORDER BY imdb_rating DESC;
+
+--select movies by language - in this case Italian AND French
+SELECT m.imdb_id, title, language, source FROM moviesandshows m
+JOIN moviesource a on m.imdb_id=a.imdb_id
+JOIN source p on p.source_id= a.source_id
+WHERE language LIKE '%French%'
+AND language LIKE '%Italian%';
+
+
